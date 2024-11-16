48648c919a8a46f8a2ed446d6db1f558c564278f
rvp15 <vedhagithub@gmail.com>
2024-11-15 22:22:12 -0500

    reviewpadding

--- src/components/Pages/Creations/Reviews.js
+++ src/components/Pages/Creations/Reviews.js
@@ -11,11 +11,11 @@ const Reviews = () => {
           <Carousel.Item key={i} >
             <Card className="mx-auto my-4 fs-5 fs-md-5 fs-lg-6 fw-normal rounded-5 fw-normal ">
               <Card.Body
-                className="px-10 my-5 "
+                className="px-10 my-5 review-txt"
                 style={{
                   color: "#dd5431",
-                  paddingLeft: "95px",
-                  paddingRight: "95px",
+                  paddingLeft: "8rem",
+                  paddingRight: "8rem",
                 }}
               >
                 <Card.Title>{item.title}</Card.Title> Reviewed By : {item.name}
